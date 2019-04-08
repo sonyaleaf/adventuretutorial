2d1a2dbbbdfe208f7330e140b172fb2128cefe85
Sonya Leaf <sleaf@servicenet.org>
2019-04-08 10:11:23 -0400

    First completed draft of tutorial

--- module tiles.py
+++ module tiles.py
@@ -5,7 +5,7 @@ Created on Fri Apr  5 14:15:09 2019
 @author: sleaf
 """
 
-import items, enemies
+import items, enemies, actions, world
  
 class MapTile:
     def __init__(self, x, y):
@@ -17,6 +17,26 @@ class MapTile:
         
     def modify_player(self, player):
         raise NotImplementedError()
+        
+    def adjacent_moves(self):
+        """Returns all move actions for adjacent tiles."""
+        moves = []
+        if world.tile_exists(self.x + 1, self.y):
+            moves.append(actions.MoveEast())
+        if world.tile_exists(self.x - 1, self.y):
+            moves.append(actions.MoveWest())
+        if world.tile_exists(self.x, self.y - 1):
+            moves.append(actions.MoveNorth())
+        if world.tile_exists(self.x, self.y + 1):
+            moves.append(actions.MoveSouth())
+        return moves
+     
+    def available_actions(self):
+        """Returns all of the available actions in this room."""
+        moves = self.adjacent_moves()
+        moves.append(actions.ViewInventory())
+     
+        return moves
         
 class StartingRoom(MapTile):
     def intro_text(self):
@@ -44,6 +64,12 @@ class EnemyRoom(MapTile):
     def modify_player(self, the_player):
         the_player.hp = the_player.hp - self.enemy.damage
         print("Enemy does {} damage. You have {} HP remaining.".format(self.enemy.damage, the_player.hp))
+        
+    def available_actions(self):
+        if self.enemy.is_alive():
+            return [actions.Flee(tile=self), actions.Attack(enemy=self.enemy)]
+        else:
+            return self.adjacent_moves()
         
 class EmptyCavePath(MapTile):
     def intro_text(self):
@@ -75,3 +101,16 @@ class FindDaggerRoom(LootRoom):
         Your notice something shiny in the corner.
         It's a dagger! You pick it up.
         """
+        
+class LeaveCaveRoom(MapTile):
+    def intro_text(self):
+        return """
+        You see a bright light in the distance...
+        ... it grows as you get closer! It's sunlight!
+ 
+ 
+        Victory is yours!
+        """
+ 
+    def modify_player(self, player):
+        player.victory = True
